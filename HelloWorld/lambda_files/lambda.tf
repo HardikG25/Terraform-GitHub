b31a0a887a9c1da91c6ff79eeb5cb2dd2bdafe8f
Hardik Grover <hardikgrover141@gmail.com>
2024-06-01 18:14:37 +0530

    Final

--- HelloWorld/lambda_files/lambda.tf
+++ HelloWorld/lambda_files/lambda.tf
@@ -1,22 +1,41 @@
 data "archive_file" "zip" {
   type        = "zip"
-  source_file = "hello_world.py"
+  source_file = "${path.module}/./hello_world.py"
   output_path = "hello_world.zip"
 }
 
 resource "aws_lambda_function" "lambda" {
   function_name = "hello_world"
 
-  filename         = "${data.archive_file.zip.output_path}"
-  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
 
-  role    = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "hello_world.hello_lambda"
+  role    = aws_iam_role.iam_for_lambda.arn
+  handler = "hello_world.lambda_handler"
   runtime = "python3.12"
 
   environment {
     variables = {
-      name = "Hello"
+      name = "World"
     }
   }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
 }
--- HelloWorld/lambda_files/python_function.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def lambda_handler(event, context):
-    return "Hello {}!".format(os.environ('name'))
